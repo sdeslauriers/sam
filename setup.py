4d165d6befada23ba55e96b6e6d55d16c4ee7188
Samuel Deslauriers-Gauthier <sam.deslauriers@gmail.com>
2018-05-25 10:51:23 +0200

    FEAT: Add main package and setup file
    This commit adds the main package and a basic setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sam',
+    version='0.0.0',
+    packages=['sam'],
+    url='https://github.com/sdeslauriers/sam',
+    license='MIT',
+    author='Samuel Deslauriers-Gauthier',
+    author_email='sam.deslauriers@gmail.com',
+    description='A Python package that contains Sam\'s python tools.'
+)
